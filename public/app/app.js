d5a062b3eb3aceefff45a5de26a36f44d00668a3
ianmin2 <ianmin2@live.com>
2016-09-23 09:47:10 +0300

    :construction: Automated project save

--- public/app/app.js
+++ public/app/app.js
@@ -6,6 +6,8 @@ var app = angular.module("myApp", ["ngRoute", "controllers",'bixApp'])
 		{ 
 			path :"/",
 			template: "/views/"
+			,menu: ""
+			,icon: ""
 			,controller: ""
 		}
 */
@@ -13,40 +15,58 @@ var app = angular.module("myApp", ["ngRoute", "controllers",'bixApp'])
 		{ 
 			path :"/manage_levels",
 			template: "views/manage_levels.html"
+			,menu: "admin"
+			,icon: ""
 		}
 		,{ 
 			path :"/manage_users",
 			template: "views/manage_users.html"
+			,menu: "admin"
+			,icon: ""
 		}
 		,{ 
 			path :"/manage_streams",
 			template: "views/manage_streams.html"
+			,menu: "admin"
+			,icon: ""
 		}
 		,{ 
 			path :"/manage_roles",
 			template: "views/manage_roles.html"
+			,menu: "admin"
+			,icon: ""
 		}
 		,{ 
 			path :"/add_teacher",
 			template: "views/add_teacher.html"
 			,controller: "TeacherController"
+			,menu: "forms"
+			,icon: ""
 		}
 		,{ 
 			path :"/add_student",
 			template: "/views/add_student.html"
 			,controller: "StudentController"
+			,menu: "forms"
+			,icon: ""
 		}
 		,{ 
 			path :"/add_classes",
 			template: "/views/add_classes.html"
+			,menu: "forms"
+			,icon: ""
 		}
 		,{ 
 			path :"/view_teacher",
 			template: "views/view_teacher.html"
+			,menu: "tables"
+			,icon: ""
 		}
 		,{ 
 			path :"/view_student",
 			template: "views/view_student.html"
+			,menu: "tables"
+			,icon: ""
 		}
 	];
 
